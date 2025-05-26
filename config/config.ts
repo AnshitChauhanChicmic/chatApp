b25b2bb64ddf3dd950f83aa8939b5b7210832f45
anshit chauhan <anshit.chauhan@chicmicstudios.in>
2025-05-26 11:14:54 +0530

    feat: added default configurations to run local

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    PORT: process.env.PORT,
-    DB_URI: process.env.DB_URI,
-    TOKEN_KEY: process.env.TOKEN_KEY
+    PORT: process.env.PORT || 5000,
+    DB_URI: process.env.DB_URI || 'mongodb://127.0.0.1:27017/chat-Application',
+    TOKEN_KEY: process.env.TOKEN_KEY || '@@@@'
 };
